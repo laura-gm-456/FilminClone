d5264c87c87f7a037919d4293616720758f52dda
juancastro000 <cdyutub@gmail.com>
2025-01-17 12:21:55 +0100

    Navbar prototype

---
+++ src/components/header/links.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Links = () => {
+  return (
+    <nav className="navbar__links">
+      <a href="/">Inicio</a>
+      <a href="/cine">Cine</a>
+      <a href="/series">Series</a>
+    </nav>
+  );
+};
+
+export default Links;
