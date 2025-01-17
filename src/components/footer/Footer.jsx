3eb81a664c2d8abfb9c68e003bd69102d257a1a2
laura-gm-456 <loragarmur@gmail.com>
2025-01-17 20:16:50 +0100

    initial changes

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+    <footer className="footerContainer"> 
+    <div className="upperLinks">
+            <a href="#">Centro de ayuda</a>
+            <a href="#">Aviso Legal</a>
+            <a href="#">Prensa</a>
+            <a href="#">¿Quiénes somos?</a>
+    </div>
+
+        <img className="teamImg" src="" alt="Erica Montesinos-GitHub" title="Erica Montesinos-GitHub"/>
+    
+    </footer>
+
+
+
+);
+
+};
+
+
+export default Footer;
+
