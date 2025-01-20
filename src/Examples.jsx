732960fcb81783b3ea7a6db4f7bac55b4f73ffd2
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-20 19:21:28 +0100

    TmbServices and call Examples for standardize call form

--- src/Examples.jsx
+++ src/Examples.jsx
@@ -1,17 +1,113 @@
 import React, { useEffect, useState } from 'react';
-import {getPopularMovies} from './services/TmbServices.jsx';
+import {getProductById, getProductsByList, getProductsBySearch, getProductsByTrendy} from './services/TmbServices.jsx';
+
+function Example(){
+  let productId=278;//id de ejemplo
+  let productList="popular";//lista de ejemplo
+  let productType="movie";//=movie or tv or all or search or person
+  let textSearch="Cadena Perpetua"; //ejemplo de búsqueda
+  let trendyTime="week";
+  let error;
+ //La idea son varios ejemplos que puedan serviros de guía de como confeccionar las funciones según necesidades, más o menos son siempre iguales
+
+
+//producto por id
+  const [product, setProduct]=useState();
+  useEffect(() => {
+    async function fetchProduct() {
+      try {        
+        setProduct(await getProductById(productType, productId)); //producto por typo e id;
+      } catch (error) {
+        console.error('Error getProductById:', error);
+        throw error; // Propaga el error para manejarlo en otro lugar si es necesario
+      }
+    }
+    fetchProduct();
+  }, []);
+  //console.log(product);
 
 
 
-const Example = () => {
-  const [movies, setMovies] = useState([]);
+
+
+
+
+//productos por lista  
+  const [list, setList]=useState();
+  useEffect(() => {
+    async function fetchList() {
+      try {
+        const data=await getProductsByList(productType, productList);
+        setList(data.results); //////////////////AQUI ES DONDE HAY QUE ELEGIR RESULTS POR EJEMPLO
+      } catch (error) {
+        console.error('Error getProductById:', error);
+        throw error; // Propaga el error para manejarlo en otro lugar si es necesario
+      }
+    }
+    fetchList();
+  }, []);
+  //console.log(result);
+  //console.log(list);
+  const movies=list;//esto e para que se vea lo de Lanny
+
+
+
+
+
+
+  
+  //productos por busqueda  
+  const [search, setSearch]=useState();
+  useEffect(() => {
+    async function fetchSearch() {
+      try {
+        setSearch(await getProductsBySearch(textSearch)); 
+      } catch (error) {
+        console.error('Error getProductById:', error);
+        throw error; // Propaga el error para manejarlo en otro lugar si es necesario
+      }
+    }
+    fetchSearch();
+  }, []);
+  console.log(search);
+
+
+
+
+
+
+  //productos por trendy  
+  const [trendy, setTrendy]=useState();
+  useEffect(() => {
+    async function fetchTrendy() {
+      try {
+        setTrendy(await getProductsByTrendy(productType, trendyTime));
+      } catch (error) {
+        console.error('Error getProductById:', error);
+      }
+    }
+    fetchTrendy();
+  }, []);
+  //console.log(trendy);
+
+
+
+
+
+  //ESTA ES LA PARTE ANTIGUA DE LANNY (con cosas mias) POR SI SE NECESITA
+
+  /*console.log(product);
+  console.log(searchByTitle);
+  console.log(movieById);
+  console.log(trendys);
+/*  const [movies, setMovies] = useState([]);
   const [error, setError] = useState('');
 
-/*load PopularMovies es una funcion asincrona que va a llamar a la funcion getPopularMovies de TMBServices.jsx
+load PopularMovies es una funcion asincrona que va a llamar a la funcion getPopularMovies de TMBServices.jsx
 y te va a dar una lista de pelivulas populares desde la API. Si la solicitud es exitosa se va a actualizarel 
 estado de movies con los datos obtenidos.
 Y si ocurre algun error(por ejemplo la API no responde ) te lo va a capturar y se actualiza el estado de 
-ese error con un mensaje*/
+ese error con un mensaje
 
   useEffect(() => {//fectching de la api
     const loadPopularMovies  = async () => {
