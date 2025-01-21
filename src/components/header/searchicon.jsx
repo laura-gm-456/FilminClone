27ef5d17bc15c03338a73b247e9d23173bdb94bd
juancastro000 <cdyutub@gmail.com>
2025-01-21 13:30:50 +0100

    fix

--- src/components/header/searchicon.jsx
+++ src/components/header/searchicon.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
-import { ReactComponent as SearchIconSVG } from "../../images/search_icon1.svg";
 import "./searchicon.css";
-
 
 const SearchIcon = () => {
   const [isSearchVisible, setIsSearchVisible] = useState(false);
@@ -12,13 +10,17 @@ const SearchIcon = () => {
 
   return (
     <div className="search-icon">
-      {/* SVG como componente React */}
-      <SearchIconSVG
-        className="search-icon__image"
-        onClick={toggleSearch}
-      />
-      
-      {/* Barra de búsqueda */}
+      <svg
+      className="search-icon"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      onClick={toggleSearch}
+    >
+      <circle cx="10" cy="10" r="7" stroke="currentColor" fill="none" strokeWidth="2" />
+      <line x1="20" y1="20" x2="14.65" y2="14.65" stroke="currentColor" strokeWidth="2" />
+    </svg>
       {isSearchVisible && (
         <div className="search-bar">
           <input
