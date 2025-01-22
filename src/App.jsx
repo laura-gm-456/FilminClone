9bcc6c2e3ce271e6ba7abd7a4da301f4ad7ffddd
juancastro000 <cdyutub@gmail.com>
2025-01-22 13:56:41 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,13 @@ import Navbar from "./components/header/navbar";
 
 const App = () => {
   return (
-    <>
-      <Example />
-    </>
-  )
-}
+    <div>
+      <Navbar />
+      <main style={{ marginTop: "60px" }}>
+        {/* Aquí va el resto de la página */}
+      </main>
+    </div>
+  );
+};
 
-
-
-export default App
-
- 
-
-
+export default App;
