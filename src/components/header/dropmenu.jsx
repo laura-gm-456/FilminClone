12b5885399e2e40b24f57c2b4aa7eee841d166d9
juancastro000 <cdyutub@gmail.com>
2025-01-22 09:26:45 +0100

    style changes

--- src/components/header/dropmenu.jsx
+++ src/components/header/dropmenu.jsx
@@ -3,21 +3,29 @@ import "./dropmenu.css";
 
 const DropdownMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isActive, setIsActive] = useState(false); // Estado para manejar el color del botón
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+    setIsActive(!isActive); // Cambia el estado de activo
   };
 
   return (
     <div className="dropdown">
-      <button className="dropdown__toggle" onClick={toggleMenu}>
+      <button 
+        className={`dropdown__toggle ${isActive ? "active" : ""}`}
+        onClick={toggleMenu}
+      >
         Más
       </button>
       {isOpen && (
         <ul className="dropdown__menu">
           <li><a href="#paginaenconstruccion">Novedades</a></li>
           <li><a href="#paginaenconstruccion">Próximamente</a></li>
-          <li><a href="https://www.thefilmintimes.com/es/">The Filmin Times</a></li>
+          {/* Opción "The Filmin Times" con clase específica para darle estilo */}
+          <li className="the-filmin-times">
+            <a href="https://www.thefilmintimes.com/es/">The Filmin Times</a>
+          </li>
           <li><a href="#https://www.filmin.es/blog">Blog</a></li>
           <li><a href="https://prensa.filmin.es/">Prensa</a></li>
           <li><a href="https://ayuda.filmin.es/es/support/home">Centro de ayuda</a></li>
--- src/components/header/navbar.jsx
+++ src/components/header/navbar.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./navbar.css";
 import Logo from "./logo";
 import Links from "./links";
 import SearchIcon from "./searchicon";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // Escuchar el desplazamiento de la página para aplicar el cambio de fondo
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) { // Cuando el scroll pase los 50px
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup cuando el componente se desmonte
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="navbar">
+    <header className={`navbar ${isScrolled ? "scrolled" : ""}`}>
       <div className="navbar__left">
         <Logo />
         <Links />
