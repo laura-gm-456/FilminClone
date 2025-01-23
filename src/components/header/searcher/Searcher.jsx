67212ca5ce1cc0a373883e789e53b6badc30cda8
erikamc99 <erikamc99@gmail.com>
2025-01-23 20:33:48 +0100

    fixed styles

--- src/components/header/searcher/Searcher.jsx
+++ src/components/header/searcher/Searcher.jsx
@@ -21,52 +21,50 @@ function Searcher() {
       console.error('Error al realizar la búsqueda:', error);
     }
   };
-
   return (
-    <div className="searcher-container">
-      <img src="../src/images/search.png" alt="lupa" className='searcher-icon'/><input
-        type="text"
-        value={query}
-        onChange={handleSearch}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setTimeout(() => setIsFocused(false), 200)}
-        placeholder="Buscar por título, género, director o reparto..."
-        className="searcher-input"
-      />
-      {query.trim() !== '' && results.length === 0 && (
-          <span className='no-results'>
-            No se han encontrado resultados
-          </span>
-        )}
-
-      {isFocused && results.length > 0 && (
-        <div className="results-container">
-          {results.map((item) => (
-            <div key={item.id} className="result-item">
-              {item.poster_path || item.profile_path ? (
-                <img
-                  src={`https://image.tmdb.org/t/p/w154${item.poster_path || item.profile_path}`}
-                  alt={item.title || item.name}
-                  className="result-image"
-                />
-              ) : (
-                <div className="result-placeholder" />
-              )}
-              <div className="result-text">
-                <h4 className="result-title">{item.title || item.name}</h4>
-                <p className="result-type">
-                  {item.media_type === 'movie'
-                    ? 'Película'
-                    : item.media_type === 'tv'
-                    ? 'Serie'
-                    : 'Persona'}
-                </p>
-              </div>
+  <div className="searcher-container">
+  <img src="../src/images/search.png" alt="lupa" className="searcher-icon" />
+    <input
+      type="text"
+      value={query}
+      onChange={handleSearch}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setTimeout(() => setIsFocused(false), 200)}
+      placeholder="Buscar por título, género, director o reparto..."
+      className="searcher-input"
+    />
+    {isFocused && results.length > 0 && (
+      <div className="results-container">
+        {results.map((item) => (
+          <div key={item.id} className="result-item">
+            {item.poster_path || item.profile_path ? (
+              <img
+                src={`https://image.tmdb.org/t/p/w154${item.poster_path || item.profile_path}`}
+                alt={item.title || item.name}
+                className="result-image"
+              />
+            ) : (
+              <div className="result-placeholder" />
+            )}
+            <div className="result-text">
+              <h4 className="result-title">{item.title || item.name}</h4>
+              <p className="result-type">
+                {item.media_type === 'movie'
+                  ? 'Película'
+                  : item.media_type === 'tv'
+                  ? 'Serie'
+                  : 'Persona'}
+              </p>
             </div>
-          ))}
-        </div>
-      )}
-    </div>
+          </div>
+        ))}
+      </div>
+    )}
+  
+  {query.trim() !== '' && results.length === 0 && (
+    <span className="no-results">No se han encontrado resultados</span>
+  )}
+</div>
   );
 }
 
